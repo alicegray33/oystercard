0022a61a25b14c7afdd44b27b0b234a9d1e00847
Alice Gray <alicegray33@gmail.com>
2022-01-04 15:52:16 +0000

    Chapter 7 complete, user story 4 done

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -17,4 +17,10 @@ describe Oystercard do
     end
   end
 
+  it { is_expected.to respond_to(:deduct).with(1).argument }
+  it 'deducts an amount from the balance' do
+    subject.top_up(20)
+    expect{ subject.deduct 3}.to change{ subject.balance }.by -3
+  end
+
 end
