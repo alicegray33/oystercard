ed383071ac8b8bc726c5f4b7d4187dd5a02fe173
Alice Gray <alicegray33@gmail.com>
2022-01-05 16:57:29 +0000

    Exercise 12 completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,17 +1,37 @@
 class Oystercard
-
+  
   MAXIMUM_BALANCE = 90
-
-  attr_reader :balance
+  MINIMUM_BALANCE = 1
+  attr_reader :balance, :journeys
 
   def initialize
     @balance = 0
+    @in_use = false
+    @journeys = Hash.new
   end
-
+  
   def top_up(amount)
     fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
+
+  def in_journey?
+    !!@in_use
+  end
+
+  def touch_in(station)
+    fail "Minimum balance of #{MINIMUM_BALANCE} required to touch in" if @balance < MINIMUM_BALANCE
+    @in_use = true
+    @journeys.store(:entry_station, station)
+  end
+
+  def touch_out(station)
+    @in_use = false
+    deduct(MINIMUM_BALANCE)
+    @journeys.store(:exit_station, station)
+  end
+
+  private
 
   def deduct(amount)
     @balance -= amount
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,26 +1,73 @@
-require 'oystercard.rb'
+require 'oystercard'
 
 describe Oystercard do
-  it 'has a balance of zero' do
-    expect(subject.balance).to eq(0)
+
+  let(:entry_station){ double :station }
+  let(:exit_station){ double :station }
+  let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
+
+  it 'starts with a balance of zero' do
+    expect(subject.balance).to eq (0)
+  end
+  it 'is initially not in a journey' do
+    expect(subject).not_to be_in_journey
+  end
+  it 'has an empty list of journeys by default' do
+    expect(subject.journeys).to be_empty
+  end
+  
+  context 'topped up and touched in:' do
+    before do
+      subject.top_up(Oystercard::MINIMUM_BALANCE)
+      subject.touch_in(entry_station)
+    end
+    it 'stores the starting station' do
+      expect(subject.journeys).to include( { entry_station: entry_station } )
+    end
+    it 'stores the exit station' do
+      subject.touch_out(exit_station)
+      expect(subject.journeys).to include( { exit_station: exit_station } )
+    end
   end
 
-  describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-    it 'can top up the balance' do
-      expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
-    end
-    it 'raises an error if the maximum balance is exceeded' do
-      maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maximum_balance)
-      expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+  it 'can top up the balance' do
+    expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
+  end
+
+  context 'has maximum balance:' do
+    before(:each) do
+      @maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(@maximum_balance)
+    end 
+    it 'raises an error if the maximun balance is exceeded' do
+      expect{ subject.top_up 1 }.to raise_error "Maximum balance of #{@maximum_balance} exceeded"
     end
   end
 
-  it { is_expected.to respond_to(:deduct).with(1).argument }
-  it 'deducts an amount from the balance' do
-    subject.top_up(20)
-    expect{ subject.deduct 3}.to change{ subject.balance }.by -3
+  context 'loaded with minumum balance and touched in:' do
+    before do
+      subject.top_up(Oystercard::MINIMUM_BALANCE)
+      subject.touch_in(entry_station)
+    end
+    it 'can be in a journey' do
+      expect(subject).to be_in_journey
+    end
+    it 'can touch out' do
+      subject.touch_out(exit_station)
+      expect(subject).not_to be_in_journey
+    end
+    it 'will deduct the correct fare at end of journey' do      
+      expect { subject.touch_out(exit_station) }.to change{ subject.balance }.by(-Oystercard::MINIMUM_BALANCE)
+    end
+    it 'stores a journey' do
+      subject.touch_out(exit_station)
+      expect(subject.journeys).to include journey
+    end  
+  end
+  
+  it 'raises an error if below minimum balance' do
+    @minimum_balance = Oystercard::MINIMUM_BALANCE
+    expect{ subject.touch_in(entry_station) }.to raise_error "Minimum balance of #{@minimum_balance} required to touch in"
   end
 
 end
